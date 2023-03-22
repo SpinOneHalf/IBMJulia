a5766e49031042ae1d4d7681433f63429fd99d80
charlestheurkauf <charlestheurkauf@outlook.com>
2023-03-22 15:13:22 -0400

    removed loops in gpu front

--- simulationtools.jl
+++ simulationtools.jl
@@ -8,18 +8,17 @@ global gamma = 1.4
 global CFL = 0.5
 
 function update!(Qnew, dt, dx, dy, nx, ny, Fh, Gh, fb)
-        @threads for j =2:ny-1
-        Qnew[2:nx-1, j, :] = Qnew[2:nx-1, j, :] - 
-        (dt / dx) * (Fh[2:nx-1, j, :] - Fh[1:nx-2, j, :]) - 
-        (dt / dy) * (Gh[2:nx-1, j, :] - Gh[2:nx-1, j-1, :]) + fb[2:nx-1,j,:] * dt
-        end
+        Qnew[2:nx-1, 2:ny-1, :] = Qnew[2:nx-1, 2:ny-1, :] - 
+        (dt / dx) * (Fh[2:nx-1, 2:ny-1, :] - Fh[1:nx-2, 2:ny-1, :]) - 
+        (dt / dy) * (Gh[2:nx-1, 2:ny-1, :] - Gh[2:nx-1, 1:ny-2, :]) + fb[2:nx-1,2:ny-1,:] * dt
+
 end
+
 
 function simulation(r, u, v, E, p, c,
                     dx, dy, ny, nx, X0, tMax,dtheta,k=.001)
     # TODO: Redesign around generic force on fluid
     t = 0
-    C=c
     Q = Array{Float32}(undef,nx, ny, 4)
     F = Array{Float32}(undef,nx, ny, 4)
     G = Array{Float32}(undef,nx, ny, 4)
@@ -33,7 +32,7 @@ function simulation(r, u, v, E, p, c,
     ks = size(xs)[1]
     tempf = Array{Float32}(undef,ks, 2)
     while t < tMax
-        dt = CFL * minimum([dx, dy]) / maximum(C.+sqrt.(u.*u+v.*v))
+        dt = CFL * minimum([dx, dy]) / maximum(c.+sqrt.(u.*u+v.*v))
         print("time is:")
         println(t)
         generateQFG!(Q,F,G,r, u, v, E, p)
@@ -56,7 +55,7 @@ function simulation(r, u, v, E, p, c,
         p = (gamma - 1) * (E - 0.5 * r .* (u .* u + v.* v))
         
         
-        C = sqrt.(gamma* p ./ r)
+        c = sqrt.(gamma* p ./ r)
         t = t + dt  # Ardvance  time
 
     end
@@ -64,73 +63,53 @@ function simulation(r, u, v, E, p, c,
 end
 
 function generateQFG!(Q,F,G,r, u, v, E, p)
-    nx, ny = size(r)
-    @threads for j = 1:ny
     # Flux calculation
-    Q[1: nx, j, 1] = r[1: nx, j]
-    Q[1: nx, j, 2] = r[1: nx, j].* u[1: nx, j]
-    Q[1: nx, j, 3] = r[1: nx, j].* v[1: nx, j]
-    Q[1: nx, j, 4] = E[1: nx, j]
+    Q[1: nx, 1:ny, 1] = r[1: nx, 1:ny]
+    Q[1: nx, 1:ny, 2] = r[1: nx, 1:ny].* u[1: nx, 1:ny]
+    Q[1: nx, 1:ny, 3] = r[1: nx, 1:ny].* v[1: nx, 1:ny]
+    Q[1: nx, 1:ny, 4] = E[1: nx, 1:ny]
 
-    F[1: nx, j, 1] = r[1: nx, j].* u[1: nx, j]
-    F[1: nx, j, 2] = r[1: nx, j].* (u[1: nx, j].^ 2) + p[1: nx, j]
-    F[1: nx, j, 3] = r[1: nx, j].* u[1: nx,j].* v[1: nx, j]
-    F[1: nx, j, 4] = u[1: nx, j].* (E[1: nx, j] + p[1: nx, j])
+    F[1: nx, 1:ny, 1] = r[1: nx, 1:ny].* u[1: nx, 1:ny]
+    F[1: nx, 1:ny, 2] = r[1: nx, 1:ny].* (u[1: nx, 1:ny].^ 2) + p[1: nx, 1:ny]
+    F[1: nx, 1:ny, 3] = r[1: nx, 1:ny].* u[1: nx,1:ny].* v[1: nx, 1:ny]
+    F[1: nx, 1:ny, 4] = u[1: nx, 1:ny].* (E[1: nx, 1:ny] + p[1: nx, 1:ny])
 
-    G[1: nx,j, 1] = r[1: nx, j].* v[1: nx, j]
-    G[1: nx, j, 2] = r[1: nx, j].* v[1: nx, j].* u[1: nx, j]
-    G[1: nx, j, 3] = r[1: nx, j].* v[1: nx, j].^ 2 + p[1: nx, j]
-    G[1: nx, j, 4] = v[1: nx, j].* (E[1: nx, j] + p[1: nx, j])
-    end
-    return Q, F, G
+    G[1: nx,1:ny, 1] = r[1: nx, 1:ny].* v[1: nx, 1:ny]
+    G[1: nx, 1:ny, 2] = r[1: nx, 1:ny].* v[1: nx, 1:ny].* u[1: nx, 1:ny]
+    G[1: nx, 1:ny, 3] = r[1: nx, 1:ny].* v[1: nx, 1:ny].^ 2 + p[1: nx, 1:ny]
+    G[1: nx, 1:ny, 4] = v[1: nx, 1:ny].* (E[1: nx, 1:ny] + p[1: nx, 1:ny])
 end
 
 function generateFhGh!(ah1,ah2,Fh,Gh,Q, F, G, c, u, v)
-    nx, ny = size(c)
-
-          @threads for i =1 :nx - 1
-            ah1[i, 1:ny-1] = max(abs.(u[i, 1: ny - 1]) + c[i, 1: ny - 1], abs.(u[i + 1, 1: ny - 1]) + c[i + 1, 1: ny - 1])
-            ah2[i, 1:ny-1] = max(abs.(v[i, 1: ny - 1]) + c[i, 1: ny - 1], abs.(v[i, 2: ny]) + c[i, 2:ny])
-
-            Fh[i, 1:ny-1, :] = 0.5 * (F[i + 1, 1:ny-1, :] + F[i, 1: ny-1, :] - ah1[i, 1:ny-1].* (Q[i + 1, 1:ny-1, :] - Q[i,1: ny-1, :]))
-            Gh[i, 1:ny-1, :] = 0.5 * (G[i, 1: ny - 1, :] + G[i, 2: ny, :] - ah2[i, 1: ny - 1].* (Q[i, 2: ny, :] - Q[i, 1: ny - 1, :]))
-        end
-    return Fh, Gh
+    ah1[1:nx-1, 1:ny-1] = max.(abs.(u[1:nx-1, 1: ny - 1]) + c[1:nx-1, 1: ny - 1], abs.(u[2:nx, 1: ny - 1]) + c[2:nx, 1: ny - 1])
+    ah2[1:nx-1, 1:ny-1] = max.(abs.(v[1:nx-1, 1: ny - 1]) + c[1:nx-1, 1: ny - 1], abs.(v[1:nx-1, 2: ny]) + c[1:nx-1, 2:ny])
+    Fh[1:nx-1, 1:ny-1, :] = 0.5 * (F[2:nx, 1:ny-1, :] + F[1:nx-1, 1: ny-1, :] - ah1[1:nx-1, 1:ny-1].* (Q[2:nx, 1:ny-1, :] - Q[1:nx-1,1: ny-1, :]))
+    Gh[1:nx-1, 1:ny-1, :] = 0.5 * (G[1:nx-1, 1: ny - 1, :] + G[1:nx-1, 2: ny, :] - ah2[1:nx-1, 1: ny - 1].* (Q[1:nx-1, 2: ny, :] - Q[1:nx-1, 1: ny - 1, :]))
 end
 
 function forceFluid!(Fb,dx, dy, nx, ny, X0, dtheta, t,k,u,v,tempf)
     Fk = _curve_force(X0, dtheta, t,k,tempf)
-    p =size(X0)[1]
     fill!(Fb,0.0)
-    ftemp = zeros((3))
-     @threads for i = 1:nx
-        ftemp = zeros((3))
-        for j = 1:ny
-            xp = i * dx
-            yp = j * dy
-            
-            for l = 1:p
-                xtemp = X0[l, :]
-                xpt = (floor(xtemp[1] / dx)% nx )* dx
-                ypt = (floor(xtemp[2] / dy)% ny) * dy
-                w = SixPointDelta((xpt - xp)/(dx))*SixPointDelta((ypt - yp)/dy) / (dx * dy)
-                Fl = w * Fk[l, :] * dtheta
-                ftemp[1:2] += Fl
-                ftemp[3]+=Fl[1]*u[i,j]+Fl[2]*v[i,j]
-            Fb[i, j, 2:4] .= ftemp
-            fill!(ftemp,0.0)
-        end
-        end
+    xp=(1:nx)*dx
+    yp=(1:ny)*dy
+    for l = 1:size(X0)[1]
+            xtemp = X0[l, :]
+            xpt = xtemp[1]
+            ypt = xtemp[2]
+            w = SixPointDelta.((xp.-xpt)./(dx))*(SixPointDelta.((yp.-ypt)./dy)')./(dx * dy)
+            Fu = w.*(Fk[l, 1] * dtheta)
+            Fv = w.*(Fk[l,2]*dtheta)
+            Fb[:,:,2] += Fu
+            Fb[:,:,3] +=Fv
+            Fb[:,:,4]+=Fu.*u+Fv.*v
     end
     return Fb
 end
 
 
-function _curve_force(xs, dthe, t, K,tempf)
+function _curve_force(xs, dthe, t, K=.1,tempf=tempf,)
     k = size(xs)[1]
-    @threads for i = 2:k - 1
-        tempf[i, :] = ((xs[i + 1,:] - 2 * xs[i, :] + xs[i - 1,:])./ (dthe ^ 2)) *K
-    end
+    tempf[2:k - 1, :] = ((xs[3:k ,:] - 2 * xs[2:k - 1, :] + xs[1:k - 2,:])./ (dthe ^ 2)) *K
     tempf[1, :] = ((xs[end, :] - 2 * xs[1, :] + xs[2, :])./ (dthe ^ 2)) * K
     tempf[end, :] = ((xs[end-1, :] - 2 * xs[end, :] + xs[1, :])./ (dthe ^2)) * K
     return tempf
@@ -144,18 +123,12 @@ function forcesBody(vs,Qnew, X0,
     r = Qnew[:, :, 1]
     u = Qnew[:, :, 2]./ r
     v = Qnew[:, :, 3]./ r
-    points_n, _ = size(X0)
     ut=linear_interpolation((x,y), u)
     vt=linear_interpolation((x,y),v)
-    @threads for i = 1:points_n
-        temp_x = X0[i, :]
-        xp = (floor(temp_x[1] / dx)% nx) * dx
-        yp = (floor(temp_x[2] / dy)% ny) * dy
-        up = ut(xp,yp)
-        vp = vt(xp,yp)
-        vs[i, 2] = up
-        vs[i, 1] = vp
-    end
+    up = ut.(X0[:,1],X0[:,2])
+    vp = vt.(X0[:,1],X0[:,2])
+    vs[:, 2] = up
+    vs[:, 1] = vp
     return vs
 end
 
--- testsimulations.jl
+++ testsimulations.jl
@@ -1,12 +1,12 @@
 include("simulationtools.jl")
 include("initalconditions.jl")
 using Plots
-nx = 250
-ny = 250
+nx = 150
+ny = 150
 r0, u0, v0, p0, E0, c0, dx, dy = static_case(nx, ny)
-xs, dtheta = generate_circle(.1, 100)
+xs, dtheta = generate_circle(.1, 300)
 
 r,u,v,E,p = simulation(r0, u0, v0, E0, p0, c0,
-                    dx, dy, ny, nx, xs, .3,dtheta)
+                    dx, dy, ny, nx, xs, .2,dtheta)
 heatmap(p)
 print("DONE")
